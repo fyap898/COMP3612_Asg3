e4f361530a82c3a9a73dfc4ee6906f4ead1058ae
fyap898 <124294533+fyap898@users.noreply.github.com>
2023-12-10 00:18:15 -0700

    update

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express();
 const artistData = require('./scripts/artists.json'); 
 const galleryData = require('./scripts/galleries.json');
+const paintingData = require('./scripts/paintings-nested.json');
 
-app.get('/api/paintings', (req,resp) => { 
+//Return all content of a json file
+app.get('/api/artists', (req,resp) => { 
     resp.json(artistData) 
 });
 app.get('/api/galleries', (req,resp) => { 
     resp.json(galleryData) 
+});
+app.get('/api/paintings', (req,resp) => { 
+    resp.json(paintingData) 
+});
+
+//Return painting id
+app.get('/api/paintings/:id', (req,resp) => { 
+    const id = req.params.id;
+
+    const pid = paintingData.find(p => p.paintingID === id);
+
+    if(pid)
+    {
+        resp.json(pid);
+    } else {
+        resp.json([{Pid: 'Invalid painting ID'}]);
+    }
 });
 
 
