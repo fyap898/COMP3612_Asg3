4dc80378588cfd57b8c7f0533f2c46984849fa12
fyap898 <124294533+fyap898@users.noreply.github.com>
2023-12-09 23:02:29 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const data = [{id:1,name:"Portrait"},{ id:1,name:"Landscape"}]; 
+app.get('/', (req,resp) => { resp.json(data) } );
+let port = 8080; app.listen(port, () => {
+    console.log("Server running at port= " + port); 
+});
