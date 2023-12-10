a753d851eacf76f15b326415b8ad2b4ff5a94567
fyap898 <124294533+fyap898@users.noreply.github.com>
2023-12-09 23:47:04 -0700

    get

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const artdata = fetch('artists.json'); 
-app.get('/', (req,resp) => { resp.json(artdata) } );
+const artistData = require('./scripts/artists.json'); 
+const galleryData = require('./scripts/galleries.json');
+
+app.get('/api/paintings', (req,resp) => { 
+    resp.json(artistData) 
+});
+app.get('/api/galleries', (req,resp) => { 
+    resp.json(galleryData) 
+});
 
 
-let port = 8080; app.listen(port, () => {
+let port = 8080; 
+app.listen(port, () => {
     console.log("Server running at port= " + port); 
 });
+
+
+
+
