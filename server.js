67eba4b5bd4659e60dcfdf9da52840077e09a0ef
fyap898 <124294533+fyap898@users.noreply.github.com>
2023-12-11 02:05:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const artistData = require('./scripts/artists.json');
 const galleryData = require('./scripts/galleries.json');
 const paintingData = require('./scripts/paintings-nested.json');
 
-//Return all content of a json file
-app.get('/api/artists', (req,resp) => { 
-    resp.json(artistData) 
-});
-app.get('/api/galleries', (req,resp) => { 
-    resp.json(galleryData) 
-});
+
+
+//Return all painting
 app.get('/api/paintings', (req,resp) => { 
     resp.json(paintingData) 
 });
@@ -29,7 +25,7 @@ app.get('/api/painting/:id', (req,resp) => {
     }
 });
 
-//Return painting/gallery
+//Return painting/gallery/id
 app.get('/api/painting/gallery/:id', (req,resp) => {
     const id = req.params.id;
         
@@ -44,7 +40,7 @@ app.get('/api/painting/gallery/:id', (req,resp) => {
     }
 });
 
-//Return painting/artist
+//Return painting/artist/id
 app.get('/api/painting/artist/:id', (req,resp) => {
     const id = req.params.id;
 
@@ -58,21 +54,72 @@ app.get('/api/painting/artist/:id', (req,resp) => {
     } 
 });
 
-//Return artist country
-app.get('/api/artists/:country', (req,resp) => {
-    const country = req.params.country.toLowerCase();
+//Return painting/year/MIN/MAX
+app.get('/api/painting/year/:min/:max', (req,resp) => {
+    const min = req.params.min;
+    const max = req.params.max;
 
-    console.log(artistData[1].Nationality.toLowerCase());
-    const acSet = artistData.filter(p => p.Nationality.toLowerCase() === country);
+    const pySet = paintingData.filter(p => ((min < p.yearOfWork) && (max > p.yearOfWork)));
+
+    if(pySet.length)
+    {
+        resp.json(pySet);
+    } else {
+        resp.json([{Painting_Year: 'Invalid years input'}]);
+    }
+});
+
+
+
+//Return all artist
+app.get('/api/artists', (req,resp) => { 
+    resp.json(artistData) 
+});
+
+//Return artist/country
+app.get('/api/artists/:country', (req,resp) => {
+    const country = req.params.country;
+    country.toLowerCase();
+
+    const acSet = artistData.filter(a => {
+        const aCountry = a.Nationality.toLowerCase();
+        return aCountry.includes(country);
+    });
 
     if(acSet.length)
     {
         resp.json(acSet);
     } else {
-        resp.json([{Artist_Country: 'Invalid country'}]);
+        resp.json([{Artist_Country: 'Invalid Artist\'s Country'}]);
     }
 
 });
+
+
+
+//Return all gallery
+app.get('/api/galleries', (req,resp) => { 
+    resp.json(galleryData) 
+});
+
+//Return galleries/country
+app.get('/api/galleries/:country', (req,resp) => {
+    const country = req.params.country;
+    country.toLowerCase();
+
+    const gcSet = galleryData.filter(g => {
+        const gCountry = g.GalleryCountry.toLowerCase();
+        return gCountry.includes(country);
+    });
+
+    if(gcSet.length)
+    {
+        resp.json(gcSet);
+    } else {
+        resp.json([{Gallery_Country: 'Invalid Gallery\'s Country'}]);
+    }
+}); 
+
 
 
 let port = 8080; 
